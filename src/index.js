5d216b103bd1f04e5ec8f0dd123fb1f61f2dcd98
pshigotskaya26 <pshigotskaya.tanya@tut.by>
2022-06-26 06:56:56 +0300

    feat: add solution of the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function reverse (n) {
-  
+ let nToString = String(n);
+ let reverseString = '';
+ let i, j;
+
+ if (nToString[0] === '-') {
+     nToString = nToString.slice(1);
+ }
+
+ if (nToString[nToString.length -1] === '0') {
+     nToString = nToString.slice(0, nToString.length -1);
+ }
+
+for (i = nToString.length -1 ; i >= 0; i--) {
+    reverseString += nToString[i];
+}
+
+return reverseString;
 }
